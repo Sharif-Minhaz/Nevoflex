66bab30a8feda3f1b8427b3bcb017a1fe5b87068
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-07-27 17:21:51 +0600

    feat: download-show section added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { DefaultContext } from "../contexts/DefaultContext";
 import styles from "../components/home/home.module.css";
 import Header from "../components/home/Header";
 import EnjoyTv from "../components/home/EnjoyTv";
+import DownloadShow from "../components/home/DownloadShow";
 
 const Home = () => {
 	const { assets, isSignedIn, plan } = useContext(DefaultContext);
@@ -10,6 +11,7 @@ const Home = () => {
 		<div className={styles.headerSection}>
 			<Header assets={assets} isSignedIn={isSignedIn} plan={plan} />
 			<EnjoyTv assets={assets} />
+			<DownloadShow assets={assets} />
 		</div>
 	);
 };
