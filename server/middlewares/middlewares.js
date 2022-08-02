727a5aeff9332dc2948467e635cddcbf0e3c425a
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-08-02 19:21:37 +0600

    Update middlewares.js

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -2,8 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const whitelist = [
+    'https://www.yoursite.com',
+    'http://127.0.0.1:5500',
+    'http://localhost:3000',
+    'http://localhost:8080'
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
 const middlewares = [
-	cors(),
+	cors(corsOptions),
 	cookieParser(),
 	express.urlencoded({ extended: true }),
 	express.json(),
